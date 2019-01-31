f25d2ecd7b422ec4c1c63d4646cb5b5f50d0657d
tonny <tonny@theinsiders.eu>
2019-01-31 12:18:30 +0100

    initial files

---
+++ src/Validator/Validator.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tonny
+ * Date: 31-1-2019
+ * Time: 10:37
+ */
+
+class Validator
+{
+    public function __construct()
+    {
+
+    }
+
+    public function say()
+    {
+        return 'Hello validator';
+    }
+}
